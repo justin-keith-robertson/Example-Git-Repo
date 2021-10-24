974dfaf0fdb63699ac404e64b742ba8106362e3a
Justin Robertson <justin.robertson@shipstation.com>
2021-10-23 20:38:32 -0500

    Added Sonic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+
+let sonic;
+
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 
 function setup() {
   createCanvas(1000, 1000);
+  sonic = loadImage('../assets/sonic.png');
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    clear();
+    image(sonic, mouseX, mouseY, 50, 50);
   } else {
     fill(getRandomNumber(255), getRandomNumber(255), getRandomNumber(255));
+    ellipse(mouseX, mouseY, 80, 80);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
